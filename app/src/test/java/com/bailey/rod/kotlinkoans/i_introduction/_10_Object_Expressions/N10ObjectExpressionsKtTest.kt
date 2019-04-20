b5526b861faa73b023063873df071484d8ce1e85
rodbailey <rodney.p.bailey@gmail.com>
2019-04-20 18:43:23 +1000

    Init commit of 10 object expressions

---
+++ app/src/test/java/com/bailey/rod/kotlinkoans/i_introduction/_10_Object_Expressions/N10ObjectExpressionsKtTest.kt
@@ -0,0 +1,12 @@
+package com.bailey.rod.kotlinkoans.i_introduction._10_Object_Expressions
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+
+class N10ObjectExpressionsKtTest {
+    @Test
+    fun testSort() {
+        assertEquals(listOf(5, 2, 1), task10())
+    }
+}
