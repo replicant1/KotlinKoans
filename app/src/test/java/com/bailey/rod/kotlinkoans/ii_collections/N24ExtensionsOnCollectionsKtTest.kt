fa6514fde0e0ebadea996541df656f2bf1f4ca93
rodbailey <rodney.p.bailey@gmail.com>
2019-04-26 23:03:53 +1000

    Init commit of 24 Extensions

---
+++ app/src/test/java/com/bailey/rod/kotlinkoans/ii_collections/N24ExtensionsOnCollectionsKtTest.kt
@@ -0,0 +1,36 @@
+package com.bailey.rod.kotlinkoans.ii_collections
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+
+class N24ExtensionsOnCollectionsKtTest {
+    @Test
+    fun testCollectionOfOneElement() {
+        doTest(listOf("a"), listOf("a"))
+    }
+
+    @Test
+    fun testEmptyCollection() {
+        doTest(null, listOf())
+    }
+
+    @Test
+    fun testSimpleCollection() {
+        doTest(listOf("a", "c"), listOf("a", "bb", "c"))
+    }
+
+    @Test
+    fun testCollectionWithEmptyStrings() {
+        doTest(listOf("", "", "", ""), listOf("", "", "", "", "a", "bb", "ccc", "dddd"))
+    }
+
+    @Test
+    fun testCollectionWithTwoGroupsOfMaximalSize() {
+        doTest(listOf("a", "c"), listOf("a", "bb", "c", "dd"))
+    }
+
+    private fun doTest(expected: Collection<String>?, argument: Collection<String>) {
+        assertEquals(expected, doSomethingStrangeWithCollection(argument), "The function 'doSomethingStrangeWithCollection' should do at least something with a collection:")
+    }
+}
