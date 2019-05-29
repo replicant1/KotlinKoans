ed2be5c8910ac9d30e9371c2557dd4df8c5d743d
rodbailey <rodney.p.bailey@gmail.com>
2019-05-29 20:51:10 +1000

    Works on all sample input

--- app/src/test/java/com/bailey/rod/hackerrank/graphs/roadsandlibs/Roads.kt
+++ app/src/test/java/com/bailey/rod/hackerrank/graphs/roadsandlibs/Roads.kt
@@ -5,9 +5,13 @@ import java.io.File
 import java.util.*
 import kotlin.collections.ArrayList
 
-const val debug = true
+const val debug = false
 
-data class City(val index: Int, val others: ArrayList<City>, var visited: Boolean)
+data class City(val index: Int, val others: ArrayList<City>, var visited: Boolean) {
+	override fun toString(): String {
+		return "City ${index}"
+	}
+}
 
 /**
  * @param numCities Number of cities. Cities have a one-based index.
@@ -17,84 +21,88 @@ data class City(val index: Int, val others: ArrayList<City>, var visited: Boolea
  */
 fun roadsAndLibraries(numCities: Int, cost_lib: Int, cost_road: Int, roads: Array<Array<Int>>): Long {
 	// Element [n] contains City with index n. Element [0] goes to waste.
-	val cityPool = ArrayList<City>()
+	val cityPool = HashMap<Int, City>()
 
 	if (debug)
 		println("numCities=${numCities}, cost_lib=${cost_lib}, cost_road=${cost_road}, #roads=${roads.size}")
 
-	for (road in roads) {
-		val fromCity = getOrCreateCity(road[0], cityPool)
-		val toCity = getOrCreateCity(road[1], cityPool)
-		fromCity.others.add(toCity)
-		toCity.others.add(fromCity)
+	if (cost_road >= cost_lib) {
+		return (cost_lib * numCities).toLong()
+	}
 
+	for (i in 1..numCities) {
 		if (debug)
-			println("Road from city${fromCity.index} to ${toCity.index}")
+			println("Creating city #${i}")
+		cityPool[i] = City(i, ArrayList(), false)
+	}
+
+	for (road in roads) {
+		val fromCity = cityPool[road[0]]
+		val toCity = cityPool[road[1]]
+		if ((fromCity != null) && (toCity != null)) {
+			fromCity.others.add(toCity)
+			toCity.others.add(fromCity)
+
+			if (debug)
+				println("Road from city ${fromCity.index} to ${toCity.index}")
+		}
 	}
 
 	var numSubgraphs = 0
+	var totalCost = 0.toLong()
 
 	do {
-		numSubgraphs++
-
-		if (debug)
-			println("Into 'do' loop: numSubgraphs incremented to ${numSubgraphs}")
-
 		var root = firstUnvisitedOrNull(cityPool)
 
-		if (debug)
+		if (debug) {
+			println("Into 'do' loop: numSubgraphs = ${numSubgraphs}")
 			println("First unvisited city is ${root}")
+		}
 
 		if (root != null) {
-			depthFirst(root)
+			numSubgraphs++
+			var numCitiesInSubgraph = traverse(root)
+			val cost = cost_lib + (cost_road * (numCitiesInSubgraph - 1))
+
+			if (debug) {
+				println("Num cities in subgraph = ${numCitiesInSubgraph}")
+				println("Cost for this subgraph = ${cost}")
+			}
+
+			totalCost += cost
 		}
 	} while (root != null)
 
-	if (debug)
-		println("numSubgraphs=${numSubgraphs}")
-
-
-	return if (cost_road >= cost_lib) {
-		(cost_lib * numCities).toLong()
-	} else {
-		val costPerSubgraph = cost_lib + (cost_road * (numCities - 1))
-		(numSubgraphs * costPerSubgraph).toLong()
-	}
+	return totalCost
 }
 
-fun firstUnvisitedOrNull(cityPool: ArrayList<City>): City? {
-	for (city in cityPool) {
-		if (!city.visited)
+fun firstUnvisitedOrNull(cityPool: HashMap<Int, City>): City? {
+	for (cityNum in cityPool.keys) {
+		val city = cityPool[cityNum]
+		if ((city != null) && !city.visited)
 			return city
 	}
 	return null
 }
 
-fun depthFirst(root: City) {
+fun traverse(root: City): Int {
 	if (debug)
-		println("depthFirst on city: ${root}")
-	for (other in root.others) {
-		depthFirst(other)
-	}
+		println("--- TRAVERSAL ON CITY ${root.index} BEGINS ---")
 	root.visited = true
-}
-
-fun getOrCreateCity(cityNum: Int, cityPool: ArrayList<City>): City {
-	val city = cityPool.getOrNull(cityNum)
-	if (city == null) {
-		val newCity = City(cityNum, ArrayList(), false)
-		cityPool[cityNum] = newCity
-		return newCity
-	} else {
-		return city
+	var citiesVisited = 1
+	for (other in root.others) {
+		if (!other.visited) {
+			citiesVisited += traverse(other)
+		}
 	}
+	return citiesVisited
 }
 
 class Roads {
 	@Test
 	fun main() {
 		val scan = Scanner(File("/Users/rodbailey/AndroidStudioProjects/KotlinKoans/app/src/test/java/com" +
-				"/bailey/rod/hackerrank/graphs/roadsandlibs/input/input00.txt"))
+				"/bailey/rod/hackerrank/graphs/roadsandlibs/input/input12.txt"))
 		val q = scan.nextLine().trim().toInt()
 		for (qItr in 1..q) {
 			val nmC_libC_road = scan.nextLine().split(" ")
